54c34c544e51bb94d4baa4623a9624de57fa062c
vicky-voin <alteneria127@gmail.com>
2025-04-29 17:43:29 -0400

    Added a computerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello!")
+function computerChoice(){
+    let choice = Math.random() * 100;
+
+    if(choice > 66)
+    {
+        return "rock";
+    }
+    if(choice > 33 && choice < 66)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
+}
+
+console.log(computerChoice())
