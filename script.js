dc383d617570f892efc1b4326bb72866cccda028
vicky-voin <alteneria127@gmail.com>
2025-04-29 17:58:05 -0400

    Added a playRound function

--- script.js
+++ script.js
@@ -15,4 +15,45 @@ function computerChoice(){
     }
 }
 
-console.log(computerChoice())
+function getHumanChoice(){
+    return prompt("Rock? Paper? Scissors?");
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    let winner = ""
+    let winningChoice = ""
+    let losingChoice = ""
+
+    if(humanChoice === computerChoice)
+    {
+        return "This is a tie! Try again!";
+    }
+    else{
+        if(humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "paper" ||
+            humanChoice === "paper" && computerChoice === "rock"
+        )
+        {
+            winner = "Human"
+            winningChoice = humanChoice
+            losingChoice = computerChoice
+            humanScore++
+        }
+        else
+        {
+            winner = "The computer"
+            winningChoice = computerChoice
+            losingChoice = humanChoice
+            computerScore++
+        }
+    }
+
+    return `${winner} is the winner! ${winningChoice} beats ${losingChoice}`
+}
+
+let humanScore = 0
+let computerScore = 0
+
+console.log(playRound(getHumanChoice(), computerChoice()))
